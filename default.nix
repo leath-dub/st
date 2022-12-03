f739694f2951b5014aec446fef3e263783119b3b
Cathal O'Grady <fierceinbattle@gmail.com>
2022-12-03 14:27:34 +0000

    added nix pkg

---
+++ default.nix
@@ -0,0 +1,34 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation rec {
+
+  name = "st-leath-dub";
+  src = ./.;
+
+  strictDeps = true;
+
+  makeFlags = [
+    "PKG_CONFIG=${stdenv.cc.targetPrefix}pkg-config"
+  ];
+
+  nativeBuildInputs = [
+    pkg-config
+    ncurses
+    fontconfig
+    freetype
+  ];
+
+  buildInputs = [
+    xorg.libX11
+    xorg.libXft
+  ];
+
+
+  preInstall = ''
+    export TERMINFO=$out/share/terminfo
+  '';
+
+  installFlags = [ "PREFIX=$(out)" ];
+
+  passthru.tests.test = nixosTests.terminal-emulators.st;
+}
