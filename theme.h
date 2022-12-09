26fa65802c9bc7fa0f824a5f6a5c8029fd068119
leath-dub <77889270+leath-dub@users.noreply.github.com>
2022-12-09 22:26:49 +0000

    edited theme file

--- theme.h
+++ theme.h
@@ -6,8 +6,10 @@ enum { black, red, green, yellow, blue, magenta, cyan, white,
 #undef bright
 #define bright(COLOR) (COLOR + 8)
 
+// {{{ everforest
+// {{{ dark hard
 #define dark_hard {                                                           \
-  [black          ] = "#2b3339", [bright(black)  ] = "#4b565c",               \
+  [black          ] = "#272e33", [bright(black)  ] = "#2e383c",               \
   [red            ] = "#e67e80", [bright(red)    ] = "#e67e80",               \
   [green          ] = "#a7c080", [bright(green)  ] = "#a7c080",               \
   [yellow         ] = "#dbbc7f", [bright(yellow) ] = "#dbbc7f",               \
@@ -16,11 +18,12 @@ enum { black, red, green, yellow, blue, magenta, cyan, white,
   [cyan           ] = "#83c092", [bright(cyan)   ] = "#83c092",               \
   [white          ] = "#d3c6aa", [bright(white)  ] = "#d3c6aa",               \
   [255            ] =         0, [foreground     ] = "#d3c6aa",               \
-  [background     ] = "#2b3339",                                              \
+  [background     ] = "#272e33",                                              \
 }
-
+// }}}
+// {{{ dark medium
 #define dark_medium {                                                         \
-  [black          ] = "#4b565c", [bright(black)  ] = "#4b565c",               \
+  [black          ] = "#232a2e", [bright(black)  ] = "#343f44",               \
   [red            ] = "#e67e80", [bright(red)    ] = "#e67e80",               \
   [green          ] = "#a7c080", [bright(green)  ] = "#a7c080",               \
   [yellow         ] = "#dbbc7f", [bright(yellow) ] = "#dbbc7f",               \
@@ -29,11 +32,12 @@ enum { black, red, green, yellow, blue, magenta, cyan, white,
   [cyan           ] = "#83c092", [bright(cyan)   ] = "#83c092",               \
   [white          ] = "#d3c6aa", [bright(white)  ] = "#d3c6aa",               \
   [255            ] =         0, [foreground     ] = "#d3c6aa",               \
-  [background     ] = "#2f383e",                                              \
+  [background     ] = "#2d353b",                                              \
 }
-
+// }}}
+// {{{ dark soft
 #define dark_soft {                                                           \
-  [black          ] = "#4b565c", [bright(black)  ] = "#4b565c",               \
+  [black          ] = "#333c43", [bright(black)  ] = "#3a464c",               \
   [red            ] = "#e67e80", [bright(red)    ] = "#e67e80",               \
   [green          ] = "#a7c080", [bright(green)  ] = "#a7c080",               \
   [yellow         ] = "#dbbc7f", [bright(yellow) ] = "#dbbc7f",               \
@@ -44,46 +48,52 @@ enum { black, red, green, yellow, blue, magenta, cyan, white,
   [255            ] =         0, [foreground     ] = "#d3c6aa",               \
   [background     ] = "#323d43",                                              \
 }
-
+// }}}
+// {{{ light hard
 #define light_hard {                                                          \
-  [black          ] = "#5c6a72", [bright(black)  ] = "#5c6a72",               \
+  [black          ] = "#1e2326", [bright(black)  ] = "#1e2326",               \
   [red            ] = "#f85552", [bright(red)    ] = "#f85552",               \
   [green          ] = "#8da101", [bright(green)  ] = "#8da101",               \
   [yellow         ] = "#dfa000", [bright(yellow) ] = "#dfa000",               \
   [blue           ] = "#3a94c5", [bright(blue)   ] = "#3a94c5",               \
   [magenta        ] = "#df69ba", [bright(magenta)] = "#df69ba",               \
   [cyan           ] = "#35a77c", [bright(cyan)   ] = "#35a77c",               \
-  [white          ] = "#dfddc8", [bright(white)  ] = "#dfddc8",               \
+  [white          ] = "#fffbef", [bright(white)  ] = "#f8f5e4",               \
   [255            ] =         0, [foreground     ] = "#5c6a72",               \
-  [background     ] = "#fff9e8",                                              \
+  [background     ] = "#fffbef",                                              \
 }
-
+// }}}
+// {{{ light medium
 #define light_medium {                                                        \
-  [black          ] = "#5c6a72", [bright(black)  ] = "#5c6a72",               \
+  [black          ] = "#1e2326", [bright(black)  ] = "#1e2326",               \
   [red            ] = "#f85552", [bright(red)    ] = "#f85552",               \
   [green          ] = "#8da101", [bright(green)  ] = "#8da101",               \
   [yellow         ] = "#dfa000", [bright(yellow) ] = "#dfa000",               \
   [blue           ] = "#3a94c5", [bright(blue)   ] = "#3a94c5",               \
   [magenta        ] = "#df69ba", [bright(magenta)] = "#df69ba",               \
   [cyan           ] = "#35a77c", [bright(cyan)   ] = "#35a77c",               \
-  [white          ] = "#dfddc8", [bright(white)  ] = "#dfddc8",               \
+  [white          ] = "#fdf6e3", [bright(white)  ] = "#f4f0d9",               \
   [255            ] =         0, [foreground     ] = "#5c6a72",               \
   [background     ] = "#fdf6e3",                                              \
 }
-
+// }}}
+// {{{ light soft
 #define light_soft {                                                        \
-  [black          ] = "#5c6a72", [bright(black)  ] = "#5c6a72",               \
+  [black          ] = "#1e2326", [bright(black)  ] = "#1e2326",               \
   [red            ] = "#f85552", [bright(red)    ] = "#f85552",               \
   [green          ] = "#8da101", [bright(green)  ] = "#8da101",               \
   [yellow         ] = "#dfa000", [bright(yellow) ] = "#dfa000",               \
   [blue           ] = "#3a94c5", [bright(blue)   ] = "#3a94c5",               \
   [magenta        ] = "#df69ba", [bright(magenta)] = "#df69ba",               \
   [cyan           ] = "#35a77c", [bright(cyan)   ] = "#35a77c",               \
-  [white          ] = "#dfddc8", [bright(white)  ] = "#dfddc8",               \
+  [white          ] = "#f8f0dc", [bright(white)  ] = "#eae4ca",               \
   [255            ] =         0, [foreground     ] = "#5c6a72",               \
-  [background     ] = "#f8f0dc",                                              \
+  [background     ] = "#f3ead3",                                              \
 }
+// }}}
+// }}}
 
+// {{{ nord
 #define nord {                                                                \
   [black          ] = "#5c6a72", [red            ] = "#f85552",               \
   [green          ] = "#8da101", [yellow         ] = "#dfa000",               \
@@ -96,9 +106,13 @@ enum { black, red, green, yellow, blue, magenta, cyan, white,
   [255            ] =         0, [foreground     ] = "#d8dee9",               \
   [background     ] = "#2e3440",                                              \
 }
+// }}}
+
+/* be sure this corresponds with theme set below */
+#define DARK
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = dark_hard;
+static const char *colorname[] = dark_medium;
 
 /*
  * Default colors (colorname index)
@@ -108,7 +122,11 @@ unsigned int defaultfg = 256;
 unsigned int defaultbg = 257;
 unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 256;
-unsigned int selectionbg = bright(black);
+#ifdef DARK
+  unsigned int selectionbg = bright(black);
+#else
+  unsigned int selectionbg = bright(white);
+#endif
 unsigned int selectionfg = 256;
 /* If 0 use selectionfg as foreground in order to have a uniform foreground-color */
 /* Else if 1 keep original foreground-color of each cell => more colors :) */
